7c60c9542b11f091f7081c7bd7ce9779e70c959f
Dragos Daian <daian.dragos@yahoo.com>
2023-04-12 18:26:10 +0200

    Change detection and polygon generation
    Use instead previous_points for change detection. This way it's no longer "laggy" when updating polygon.
    When generating polygon, only use outside points for polygon generation(not internal ones). Also, when generating, make sure generated polygon is inside outline.

--- addons/auto_polygon2d_triangulation/auto_poly_plugin.gd
+++ addons/auto_polygon2d_triangulation/auto_poly_plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 var editor_selection := get_editor_interface().get_selection()
 var selected_polygon2d : Polygon2D = null
-var current_polygon_count : int = 0
+var previous_points : PackedVector2Array
 
 
 func _enter_tree() -> void:
@@ -12,10 +12,11 @@ func _enter_tree() -> void:
 
 
 func _process(_delta: float) -> void:
-	if selected_polygon2d :
-		if selected_polygon2d.polygon.size() != current_polygon_count:
-			triangulate_polygons(selected_polygon2d)
-			current_polygon_count = selected_polygon2d.polygon.size()
+	if selected_polygon2d == null:
+		return
+	if selected_polygon2d.polygon != previous_points:
+		triangulate_polygons(selected_polygon2d)
+	previous_points = selected_polygon2d.polygon.duplicate()
 
 
 
@@ -28,10 +29,14 @@ func _on_selection_changed() -> void:
 
 	if not selected_nodes.is_empty() and selected_nodes[0] is Polygon2D:
 		selected_polygon2d = selected_nodes[0]
-		current_polygon_count = selected_polygon2d.polygon.size()
+		triangulate_polygons(selected_polygon2d)
 	else:
 		selected_polygon2d = null
-		current_polygon_count = 0
+
+func _is_line_in_polygon(a: Vector2, b: Vector2, polygon_verts: PackedVector2Array) -> bool:
+	return Geometry2D.is_point_in_polygon(a + a.direction_to(b) * 0.01, polygon_verts) \
+		and Geometry2D.is_point_in_polygon(b + b.direction_to(a) * 0.01, polygon_verts) \
+		and Geometry2D.is_point_in_polygon((a + b) / 2, polygon_verts)
 
 func triangulate_polygons(polygon2d : Polygon2D) -> void:
 	if polygon2d.polygon.size() < 3:
@@ -39,14 +44,24 @@ func triangulate_polygons(polygon2d : Polygon2D) -> void:
 		return
 
 	polygon2d.polygons = []
-
-	var delaunay_points = Geometry2D.triangulate_delaunay(polygon2d.polygon)
-
-	for point in range(0, delaunay_points.size(), 3):
+	var points = Geometry2D.triangulate_delaunay(polygon2d.polygon)
+	var contour_polygon = polygon2d.polygon.slice(0, polygon2d.polygon.size() - polygon2d.internal_vertex_count)
+	for point in range(0, points.size(), 3):
 		var triangle = []
-		triangle.push_back(delaunay_points[point])
-		triangle.push_back(delaunay_points[point + 1])
-		triangle.push_back(delaunay_points[point + 2])
-
-		polygon2d.polygons.push_back(triangle)
+		triangle.push_back(points[point])
+		triangle.push_back(points[point + 1])
+		triangle.push_back(points[point + 2])
+		var a = polygon2d.polygon[points[point]]
+		var b = polygon2d.polygon[points[point + 1]]
+		var c = polygon2d.polygon[points[point + 2]]
+		var center = (a + b + c) / 3
+		# move points inside the triangle so we don't check for intersection with polygon edge
+		a = a - (a - center).normalized() * 0.01
+		b = b - (b - center).normalized() * 0.01
+		c = c - (c - center).normalized() * 0.01
+		# if all same points are inside polygon, then add it
+		if Geometry2D.is_point_in_polygon(a, contour_polygon) \
+			and Geometry2D.is_point_in_polygon(b, contour_polygon) \
+			and Geometry2D.is_point_in_polygon(c, contour_polygon):
+			polygon2d.polygons.push_back(triangle)
 
