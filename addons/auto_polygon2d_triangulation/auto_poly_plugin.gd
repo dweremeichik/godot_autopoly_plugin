fed79e9930eb0d15f40281db01b17fa907d394a7
Dylan Weremeichik <dylan.weremeichik@gmail.com>
2023-04-13 03:58:44 -0400

    Integrate PR
    Remove unnecessary function
    Make triangulation more readable
    Add comments to make code more clear
    Add contributor to authors list

--- addons/auto_polygon2d_triangulation/auto_poly_plugin.gd
+++ addons/auto_polygon2d_triangulation/auto_poly_plugin.gd
@@ -19,7 +19,6 @@ func _process(_delta: float) -> void:
 	previous_points = selected_polygon2d.polygon.duplicate()
 
 
-
 func _exit_tree() -> void:
 	editor_selection.selection_changed.disconnect(self._on_selection_changed)
 
@@ -33,10 +32,18 @@ func _on_selection_changed() -> void:
 	else:
 		selected_polygon2d = null
 
-func _is_line_in_polygon(a: Vector2, b: Vector2, polygon_verts: PackedVector2Array) -> bool:
-	return Geometry2D.is_point_in_polygon(a + a.direction_to(b) * 0.01, polygon_verts) \
-		and Geometry2D.is_point_in_polygon(b + b.direction_to(a) * 0.01, polygon_verts) \
-		and Geometry2D.is_point_in_polygon((a + b) / 2, polygon_verts)
+
+func _points_are_inside_polygon(a: Vector2, b: Vector2, c: Vector2, polygon: PackedVector2Array) -> bool:
+	var center = (a + b + c) / 3
+	# move points inside the triangle so we don't check for intersection with polygon edge
+	a = a - (a - center).normalized() * 0.01
+	b = b - (b - center).normalized() * 0.01
+	c = c - (c - center).normalized() * 0.01
+	
+	return Geometry2D.is_point_in_polygon(a, polygon) \
+		and Geometry2D.is_point_in_polygon(b, polygon) \
+		and Geometry2D.is_point_in_polygon(c, polygon)
+
 
 func triangulate_polygons(polygon2d : Polygon2D) -> void:
 	if polygon2d.polygon.size() < 3:
@@ -45,23 +52,19 @@ func triangulate_polygons(polygon2d : Polygon2D) -> void:
 
 	polygon2d.polygons = []
 	var points = Geometry2D.triangulate_delaunay(polygon2d.polygon)
-	var contour_polygon = polygon2d.polygon.slice(0, polygon2d.polygon.size() - polygon2d.internal_vertex_count)
+	# Outer verticies are stored at the beginning of the PackedVector2Array
+	var outer_polygon = polygon2d.polygon.slice(0, polygon2d.polygon.size() - polygon2d.internal_vertex_count)
 	for point in range(0, points.size(), 3):
 		var triangle = []
 		triangle.push_back(points[point])
 		triangle.push_back(points[point + 1])
 		triangle.push_back(points[point + 2])
-		var a = polygon2d.polygon[points[point]]
-		var b = polygon2d.polygon[points[point + 1]]
-		var c = polygon2d.polygon[points[point + 2]]
-		var center = (a + b + c) / 3
-		# move points inside the triangle so we don't check for intersection with polygon edge
-		a = a - (a - center).normalized() * 0.01
-		b = b - (b - center).normalized() * 0.01
-		c = c - (c - center).normalized() * 0.01
-		# if all same points are inside polygon, then add it
-		if Geometry2D.is_point_in_polygon(a, contour_polygon) \
-			and Geometry2D.is_point_in_polygon(b, contour_polygon) \
-			and Geometry2D.is_point_in_polygon(c, contour_polygon):
+		
+		# only add the triangle if all points are inside the polygon
+		var a : Vector2 = polygon2d.polygon[points[point]]
+		var b : Vector2 = polygon2d.polygon[points[point + 1]]
+		var c : Vector2 = polygon2d.polygon[points[point + 2]]
+		
+		if _points_are_inside_polygon(a, b, c, outer_polygon):
 			polygon2d.polygons.push_back(triangle)
 
